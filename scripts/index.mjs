ba55cc1201abd9b7a0e6812cf674146cfcbe31d7
qinghuanI <1062217965@qq.com>
2023-06-13 03:17:08 +0000

    add blog list to readme.md

---
+++ scripts/index.mjs
@@ -0,0 +1,53 @@
+import { extname, join } from "node:path";
+import { readdir, writeFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
+const __dirname = join(fileURLToPath(import.meta.url), "..");
+
+let blogFiles = [];
+
+const MD_EXTNAME = ".md";
+
+const resolve = (dir) => join(__dirname, "..", dir);
+
+try {
+  const files = await readdir(resolve("/blog/_posts"));
+  for (const file of files) {
+    if (extname(file) === MD_EXTNAME) {
+      blogFiles.push(file);
+    }
+  }
+} catch (err) {
+  console.error("failed to read _posts dir", err);
+}
+
+function convertBlogFilesMap(files) {
+  return files
+    .reduce((acc, cur) => {
+      let date = cur.slice(0, 10).split("-").join("/");
+
+      let blogName = cur.slice(11, cur.length - 3);
+      acc.push({ date, blogName });
+      return acc;
+    }, [])
+    .reverse();
+}
+
+function blogToReadme(files) {
+  return convertBlogFilesMap(files)
+    .map(({ date, blogName }) => {
+      return `- [${blogName}](https://www.qinghuani.fun/${date}/${encodeURI(blogName)}) — ${date}\n`;
+    })
+    .join("");
+}
+
+async function writeBlogToReadme(files) {
+  // console.log(blogToReadme(blogFiles));
+  try {
+    await writeFile(resolve("readme.md"), blogToReadme(files));
+  } catch (e) {
+    console.error("failed to write blogs to readme.md", e);
+  }
+}
+
+void writeBlogToReadme(blogFiles);
